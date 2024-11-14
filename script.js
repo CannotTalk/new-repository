ca1052e7d1cc6a65db1d8b1a50773f08cf205eb9
CannotTalk <168893556+CannotTalk@users.noreply.github.com>
2024-11-14 21:34:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+{
+    let virusLeft = Math.trunc(Math.random() * (300 - 240) + 240);
+    setInterval(function(){
+        document.getElementById("virusTimeLeft").innerHTML = virusLeft;
+        virusLeft--;
+    },1000);
+}
 
+function openByClickVirusButton(){
+    window.open("https://beinternetawesome.withgoogle.com/ja_jp/interland","_blank")
+}
